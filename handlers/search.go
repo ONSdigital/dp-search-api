6cbbee9d730ddcf6d5c4cac25f1ea113c7f4b039
guido4f <james.fawke@g4pas.co.uk>
2017-03-24 11:05:49 +0000

    Added AtoZ changes

--- handlers/search.go
+++ handlers/search.go
@@ -9,17 +9,21 @@ import (
 	"github.com/tdewolff/minify/js"
 	"github.com/tdewolff/minify"
 	"regexp"
+	"net/url"
 	elasticsearch "github.com/ONSdigital/dp-search-query/elasticsearch"
 )
 
 type SearchRequest struct {
-	Term    string
-	From    int
-	Size    int
-	Types   []string
-	Index   string
-	Queries []string
-	SortBy  string
+	Term                string
+	From                int
+	Size                int
+	Types               []string
+	Index               string
+	Queries             []string
+	SortBy              string
+	AggregationField    string
+	FilterOnLatest      bool
+	FilterOnFirstLetter string
 }
 
 //Load the templates once, the main entry point for the templates is search.tmpl. The search.tmpl takes the SearchRequest struct and
@@ -35,6 +39,9 @@ var searchTemplates, _ = template.ParseFiles("templates/search/search.tmpl",
 	"templates/search/departmentsHeader.tmpl",
 	"templates/search/departmentsQuery.tmpl",
 	"templates/search/coreQuery.tmpl",
+	"templates/search/weightedQuery.tmpl",
+	"templates/search/countFilterLatest.tmpl",
+	"templates/search/contentFilters.tmpl",
 	"templates/search/sortByTitle.tmpl",
 	"templates/search/sortByRelevance.tmpl",
 	"templates/search/sortByReleaseDate.tmpl",
@@ -50,14 +57,13 @@ func (sr SearchRequest) HasQuery(query string) bool {
 	return false
 }
 
-
 func formatMultiQuery(rawQuery []byte) []byte {
 	//Is minify thread Safe? can I put this as a global?
 	m := minify.New()
 	m.AddFuncRegexp(regexp.MustCompile("[/+]js$"), js.Minify)
 
+	linearQuery, err := m.Bytes("application/js", rawQuery)
 
-	linearQuery, err := m.Bytes("application/js",rawQuery)
 
 	if err != nil {
 		panic(err)
@@ -68,11 +74,27 @@ func formatMultiQuery(rawQuery []byte) []byte {
 
 }
 
+func paramGet(params url.Values, key string, defaultValue string) string {
+	value := params.Get(key)
+	if len(value) < 1 {
+		value = defaultValue;
+	}
+	return value
+}
+
+func paramGetBool(params url.Values, key string, defaultValue bool) bool {
+	value := params.Get(key)
+	if len(value) < 1 {
+		return defaultValue;
+	}
+	return value == "true"
+}
+
 func SearchHandler(w http.ResponseWriter, req *http.Request) {
 
 	params := req.URL.Query()
-	size, err := strconv.Atoi(params.Get("size"))
-	from, err := strconv.Atoi(params.Get("from"))
+	size, err := strconv.Atoi(paramGet(params, "size", "10"))
+	from, err := strconv.Atoi(paramGet(params, "from", "0"))
 	var queries []string
 
 	if nil == params["query"] {
@@ -80,16 +102,20 @@ func SearchHandler(w http.ResponseWriter, req *http.Request) {
 	} else {
 		queries = params["query"]
 	}
-	log.Debug("SearchHandler", log.Data{"queries":queries})
+
 
 	reqParams := SearchRequest{Term: params.Get("term"),
 		From: from,
 		Size: size,
 		Types: params["type"],
 		Index: params.Get("index"),
-		SortBy: params.Get("sort"),
-		Queries: queries}
-
+		SortBy: paramGet(params, "sort", "relevance"),
+		Queries: queries,
+		AggregationField: paramGet(params, "aggField", "_type"),
+		FilterOnLatest: paramGetBool(params, "latest", false),
+		FilterOnFirstLetter: params.Get("withFirstLetter"),
+	}
+	log.Debug("SearchHandler", log.Data{"queries":queries,"request":reqParams})
 	var doc bytes.Buffer
 	err = searchTemplates.Execute(&doc, reqParams)
 
