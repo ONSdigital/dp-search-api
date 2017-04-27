38963d9f989a6cc173901e0c2a44d5698b4ca660
Geraint Edwards <geraint.edwards@ons.gov.uk>
2017-04-27 10:36:27 +0100

    tidyup wait script

--- waitForElastic.sh
+++ waitForElastic.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+
 COUNTER=0
 if [ -z $ELASTIC_URL ]; then
     ELASTIC_URL=localhost:9200
 fi
 printf "waiting for $ELASTIC_URL   "
-until $(curl --output /dev/null --silent --head --fail $ELASTIC_URL); do
+until curl --output /dev/null --silent --head --fail $ELASTIC_URL; do
     printf '.'
-    sleep 5
     if [ $COUNTER -gt 10 ]; then
-        printf "\n waited too long"
-        exit
+        printf "\n waited too long\n"
+        exit 2
     fi
+    sleep 5
     let COUNTER=COUNTER+1
 done
