b7d98cd078540767b92d3b01b539865327af8dfb
Geraint Edwards <geraint.edwards@ons.gov.uk>
2017-06-05 14:00:03 +0100

    change ci build script to copy additional files (templates/ dir)

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -5,5 +5,5 @@ cwd=$(pwd)
 export GOPATH=$cwd/go
 
 pushd $GOPATH/src/github.com/ONSdigital/dp-search-query
-  make build && mv build/$(go env GOOS)-$(go env GOARCH)/bin/* $cwd/build
+  make build && mv build/$(go env GOOS)-$(go env GOARCH)/* $cwd/build
 popd
