0243d45d300f4224935cf58461f005f25b4d2a9e
Jessica Jenkins <jess.jenkins@ons.gov.uk>
2019-11-01 11:34:07 +0000

    Copy templates into docker image on build

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -6,5 +6,6 @@ export GOPATH=$cwd/go
 
 pushd $GOPATH/src/github.com/ONSdigital/dp-search-query
   make build && mv build/$(go env GOOS)-$(go env GOARCH)/* $cwd/build
+  cp -r templates $cwd/build
   cp Dockerfile.concourse $cwd/build
 popd
