fd7ef36bea7326d9671ef4f14aa89be7c5bff50a
Noelle <noelle.legrain@gmail.com>
2021-11-22 13:05:55 +0000

    add test for setting up templates

--- query/query_test.go
+++ query/query_test.go
@@ -1,34 +1,17 @@
 package query
 
 import (
-	"github.com/pkg/errors"
-	"testing"
-	"text/template"
-
 	. "github.com/smartystreets/goconvey/convey"
+	"testing"
 )
 
-func MockNewQueryBuilder() (*Builder, error) {
-	searchTemplates, err := setupSearchTestTemplates("dummy{{.Moo}}")
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to load search templates")
-	}
-	return &Builder{
-		searchTemplates: searchTemplates,
-	}, nil
-}
-func setupSearchTestTemplates(rawTemplate string) (*template.Template, error) {
-	templates, err := template.ParseFiles(rawTemplate)
-	return templates, err
-}
+func TestSetupSearch(t *testing.T) {
+	Convey("Should return templates and error should be nil", t, func() {
+		searchTemplates, err := SetupSearch()
 
-func TestMockNewQueryBuilder(t *testing.T) {
-	Convey("Should return InternalError for unable to load template", t, func() {
+		So(err, ShouldBeNil)
+		So(searchTemplates, ShouldNotBeNil)
 
-		query, err := MockNewQueryBuilder()
-
-		So(err, ShouldNotBeNil)
-		So(query, ShouldBeNil)
-		So(err.Error(), ShouldContainSubstring, "failed to load search templates")
 	})
 }
+
