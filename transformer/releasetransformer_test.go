e8d0116774ac182b091e52ffb075f9c6ea966151
PatrickConnollyONS <Patrick.Connolly@ext.ons.gov.uk>
2022-03-15 12:55:49 +0000

    Add a new separate definition for release search results, with a corresponding transformer. Note certain definitions are replicated here to try to isolate the 'release search' from the main search

---
+++ transformer/releasetransformer_test.go
@@ -0,0 +1,55 @@
+package transformer
+
+import (
+	"context"
+	"encoding/json"
+	"os"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestTransformSearchReleaseResponse(t *testing.T) {
+	Convey("With a transformer initialised", t, func() {
+		ctx := context.Background()
+		transformer := NewReleaseTransformer()
+		So(t, ShouldNotBeNil)
+
+		Convey("Throws error on invalid JSON", func() {
+			sampleResponse := []byte(`{"invalid":"json"`)
+			_, err := transformer.TransformSearchResponse(ctx, sampleResponse, "test-query", true)
+			So(err, ShouldNotBeNil)
+			So(err.Error(), ShouldResemble, "Failed to decode elastic search response: unexpected end of JSON input")
+		})
+
+		Convey("Converts an example response with highlighting", func() {
+			sampleResponse, err := os.ReadFile("testdata/search_release_es_response.json")
+			So(err, ShouldBeNil)
+			expected, err := os.ReadFile("testdata/search_release_expected_highlighted.json")
+			So(err, ShouldBeNil)
+
+			actual, err := transformer.TransformSearchResponse(ctx, sampleResponse, "test-query", true)
+			So(err, ShouldBeNil)
+			So(actual, ShouldNotBeEmpty)
+			var exp, act SearchReleaseResponse
+			So(json.Unmarshal(expected, &exp), ShouldBeNil)
+			So(json.Unmarshal(actual, &act), ShouldBeNil)
+			So(act, ShouldResemble, exp)
+		})
+
+		Convey("Converts an example response without highlighting", func() {
+			sampleResponse, err := os.ReadFile("testdata/search_release_es_response.json")
+			So(err, ShouldBeNil)
+			expected, err := os.ReadFile("testdata/search_release_expected_plain.json")
+			So(err, ShouldBeNil)
+
+			actual, err := transformer.TransformSearchResponse(ctx, sampleResponse, "test-query", false)
+			So(err, ShouldBeNil)
+			So(actual, ShouldNotBeEmpty)
+			var exp, act SearchResponse
+			So(json.Unmarshal(expected, &exp), ShouldBeNil)
+			So(json.Unmarshal(actual, &act), ShouldBeNil)
+			So(act, ShouldResemble, exp)
+		})
+	})
+}
