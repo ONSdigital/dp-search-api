c7e59227ad41146c2cbad6ba852b60aba0cef2f8
Nathan Shumoogum <nathan.shumoogum@methods.co.uk>
2022-11-29 17:35:11 +0000

    Create Go SDK client for search API

---
+++ sdk/options.go
@@ -0,0 +1,27 @@
+package sdk
+
+import (
+	"net/http"
+	"net/url"
+)
+
+type header string
+
+const (
+	// List of available headers
+	Authorisation header = "Authorisation"
+)
+
+// Options is a struct containing for customised options for the API client
+type Options struct {
+	Headers map[header][]string
+	Query   url.Values
+}
+
+func setHeaders(req *http.Request, headers map[header][]string) {
+	for h := range headers {
+		for i := range h {
+			req.Header.Add(string(h), headers[h][i])
+		}
+	}
+}
