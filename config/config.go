8851dce6fb7dc82e1f4f6f4b757dbf5d95f9e3be
kamen.dimitrov1 <kamen.dimitrov@flaxandteal.co.uk>
2023-09-01 12:48:44 +0300

    fixed some issues with external config variables

--- config/config.go
+++ config/config.go
@@ -30,11 +30,11 @@ type AWS struct {
 
 type NLP struct {
 	NlpHubSettings      string `envconfig:"NLP_HUB_SETTINGS"`
-	BerlinAPIEndpoint   string `envconfig:"NLP_CATEGORY_API_URL"`
+	BerlinAPIEndpoint   string `envconfig:"NLP_BERLIN_API_ENDPOINT"`
 	BerlinAPIURL        string `envconfig:"NLP_BERLIN_API_URL"`
-	ScrubberAPIEndpoint string `envconfig:"NLP_CATEGORY_API_URL"`
+	ScrubberAPIEndpoint string `envconfig:"NLP_SCRUBBER_API_ENDPOINT"`
 	ScrubberAPIURL      string `envconfig:"NLP_SCRUBBER_API_URL"`
-	CategoryAPIEndpoint string `envconfig:"NLP_CATEGORY_API_URL"`
+	CategoryAPIEndpoint string `envconfig:"NLP_CATEGORY_API_ENDPOINT"`
 	CategoryAPIURL      string `envconfig:"NLP_CATEGORY_API_URL"`
 }
 
