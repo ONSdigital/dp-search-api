5c73237cbac742206a734785309ec664ea88d8fd
Jessica Jenkins <jess.jenkins@ons.gov.uk>
2025-09-05 17:10:13 +0100

    fix error handling in script

--- scripts/top-search-queries/get-top-search-results.go
+++ scripts/top-search-queries/get-top-search-results.go
@@ -5,7 +5,6 @@ import (
 	"context"
 	"encoding/csv"
 	"flag"
-	"fmt"
 	"net/url"
 	"os"
 	"strconv"
@@ -57,45 +56,85 @@ func main() {
 	flag.Parse()
 	logData := log.Data{"config: ": config}
 	log.Info(ctx, "parsed config", logData)
-	run(ctx, config)
+	err := run(ctx, config)
+	if err != nil {
+		log.Fatal(ctx, "error running", err)
+		os.Exit(1)
+	}
 	log.Info(ctx, "end of script")
 }
 
-func run(ctx context.Context, config runConfig) {
+func run(ctx context.Context, config runConfig) error {
 	listOfQueries, err := readQueriesFromFile(ctx, config)
-	check(ctx, "failed reading queries file", err)
+	if err != nil {
+		log.Error(ctx, "error reading queries from file", err)
+		return err
+	}
 
-	csvFile := createOutputCSVFile(ctx)
+	csvFile, err := createOutputCSVFile(ctx)
+	if err != nil {
+		log.Error(ctx, "error creating output csv file", err)
+		return err
+	}
 	defer func() {
 		if err = csvFile.Close(); err != nil {
-			log.Fatal(ctx, "failed closing csv file", err)
+			log.Error(ctx, "failed closing csv file", err)
+			return
 		}
 	}()
 
 	log.Info(ctx, "making each request to the Search API with NLP off initially, then NLP on")
 	for _, query := range listOfQueries {
-		searchResp := getQueryResults(ctx, query, "false", config)
-		AddResultsToCSV(ctx, query, csvFile, "false", searchResp)
-		searchResp = getQueryResults(ctx, query, "true", config)
-		AddResultsToCSV(ctx, query, csvFile, "true", searchResp)
+		searchResp, err := getQueryResults(ctx, query, "false", config)
+		if err != nil {
+			log.Error(ctx, "error getting query results", err, log.Data{"query": query})
+			return err
+		}
+		err = AddResultsToCSV(ctx, query, csvFile, "false", searchResp)
+		if err != nil {
+			log.Error(ctx, "error adding results to csv", err)
+			return err
+		}
+		searchResp, err = getQueryResults(ctx, query, "true", config)
+		if err != nil {
+			log.Error(ctx, "error getting query results", err, log.Data{"query": query})
+			return err
+		}
+		err = AddResultsToCSV(ctx, query, csvFile, "true", searchResp)
+		if err != nil {
+			log.Error(ctx, "error adding results to csv", err)
+			return err
+		}
 	}
+	return nil
 }
 
 // createOutputCSVFile creates the CSV file ready for the query results to be added to. It firstly adds a header row.
-func createOutputCSVFile(ctx context.Context) *os.File {
+func createOutputCSVFile(ctx context.Context) (*os.File, error) {
 	logData := log.Data{"output_filename": "top-search-query-results.csv"}
 	csvFile, err := os.Create("top-search-query-results.csv")
-	check(ctx, "failed creating file", err)
+	if err != nil {
+		log.Error(ctx, "error creating csv file", err)
+		return nil, err
+	}
 	log.Info(ctx, "successfully created csv file", logData)
-	writeHeaderRow(csvFile, ctx)
-	return csvFile
+	err = writeHeaderRow(csvFile, ctx)
+	if err != nil {
+		log.Error(ctx, "error writing header row to csv file", err)
+		return nil, err
+	}
+	return csvFile, nil
 }
 
 // getQueryResults calls the Search API for a particular query string and NLP weighting (true or false)
 // If the NLP weighting is true then NLP is used, if false then NLP is not used. The results of the query call are each
 // written to the supplied output CSV file as separate rows.
-func getQueryResults(ctx context.Context, querySupplied, nlpWeighting string, cfg runConfig) searchResponse {
-	sdkItems := callSearchAPI(ctx, querySupplied, nlpWeighting, cfg.APIURL, cfg.NumResults)
+func getQueryResults(ctx context.Context, querySupplied, nlpWeighting string, cfg runConfig) (searchResponse, error) {
+	sdkItems, err := callSearchAPI(ctx, querySupplied, nlpWeighting, cfg.APIURL, cfg.NumResults)
+	if err != nil {
+		log.Error(ctx, "error calling SearchAPI", err)
+		return searchResponse{}, err
+	}
 
 	response := searchResponse{
 		Items: make([]searchItem, len(sdkItems)),
@@ -110,11 +149,11 @@ func getQueryResults(ctx context.Context, querySupplied, nlpWeighting string, cf
 		response.Items[i].ReleaseDate = sdkItems[i].ReleaseDate
 	}
 
-	return response
+	return response, nil
 }
 
 // AddResultsToCSV writes the results of a Search API query call to the supplied output CSV file (as separate rows).
-func AddResultsToCSV(ctx context.Context, querySupplied string, csvFile *os.File, nlpWeighting string, responseObject searchResponse) {
+func AddResultsToCSV(ctx context.Context, querySupplied string, csvFile *os.File, nlpWeighting string, responseObject searchResponse) error {
 	now := time.Now()
 	dateTimeRequest := now.Format(time.DateTime)
 	nlpOnOrOff := "off"
@@ -127,12 +166,17 @@ func AddResultsToCSV(ctx context.Context, querySupplied string, csvFile *os.File
 	// The number of items in the responseObject will default to 10, but will be whatever is specified by the num_results input parameter, unless fewer are returned.
 	for i := 0; i < len(responseObject.Items); i++ {
 		item := responseObject.Items[i]
-		addItemToCSV(ctx, querySupplied, nlpOnOrOff, dateTimeRequest, item, i, csvWriter)
+		err := addItemToCSV(querySupplied, nlpOnOrOff, dateTimeRequest, item, i, csvWriter)
+		if err != nil {
+			log.Error(ctx, "error adding item to csv", err)
+			return err
+		}
 	}
+	return nil
 }
 
 // addItemToCSV adds a row of data, taken from one item of the results (of a particular query), to the output CSV file.
-func addItemToCSV(ctx context.Context, querySupplied, nlpOnOrOff, dateTimeRequest string, item searchItem, position int, csvWriter *csv.Writer) {
+func addItemToCSV(querySupplied, nlpOnOrOff, dateTimeRequest string, item searchItem, position int, csvWriter *csv.Writer) error {
 	rowNum := position + 1
 	resultsRow := []string{
 		nlpOnOrOff,
@@ -147,13 +191,12 @@ func addItemToCSV(ctx context.Context, querySupplied, nlpOnOrOff, dateTimeReques
 		item.Summary,
 	}
 
-	err := csvWriter.Write(resultsRow)
-	check(ctx, fmt.Sprintf("failed writing results row for query '%s' at row %d", querySupplied, rowNum), err)
+	return csvWriter.Write(resultsRow)
 }
 
 // callSearchAPI calls the live Search API using the supplied values of query and nlpWeighting for the relevant query
 // parameters. It specifies a limit of 10 results to be returned in the query response.
-func callSearchAPI(ctx context.Context, query, nlpWeighting, apiURL string, numResults int) []models.Item {
+func callSearchAPI(ctx context.Context, query, nlpWeighting, apiURL string, numResults int) ([]models.Item, error) {
 	queryVals := url.Values{}
 	queryVals.Add("q", query)
 	queryVals.Add("limit", strconv.Itoa(numResults))
@@ -166,10 +209,11 @@ func callSearchAPI(ctx context.Context, query, nlpWeighting, apiURL string, numR
 	searchAPIClient := sdk.New(apiURL)
 	response, err := searchAPIClient.GetSearch(ctx, options)
 	if err != nil {
-		log.Fatal(ctx, "failed calling Search API", err, logData)
+		log.Error(ctx, "failed calling Search API", err, logData)
+		return nil, err
 	}
 
-	return response.Items
+	return response.Items, nil
 }
 
 // readQueriesFromFile opens the text file named "search-queries.txt", in the local directory, and reads it into a
@@ -178,16 +222,23 @@ func readQueriesFromFile(ctx context.Context, cfg runConfig) (listOfQueries []st
 	logData := log.Data{"file_name": cfg.InputFileName}
 	file, err := os.Open(cfg.InputFileName)
 	if err != nil {
-		log.Fatal(ctx, "failed opening input file", err, logData)
+		log.Error(ctx, "failed opening input file", err, logData)
+		return nil, err
 	}
-	defer file.Close()
+	defer func() {
+		err := file.Close()
+		if err != nil {
+			log.Error(ctx, "failed closing input file", err, logData)
+		}
+	}()
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		listOfQueries = append(listOfQueries, strings.TrimSpace(scanner.Text()))
 	}
 	if err := scanner.Err(); err != nil {
-		log.Fatal(ctx, "failed reading input file", err, logData)
+		log.Error(ctx, "failed reading input file", err, logData)
+		return nil, err
 	}
 
 	log.Info(ctx, "queries read from input file", log.Data{"queries": listOfQueries})
@@ -195,7 +246,7 @@ func readQueriesFromFile(ctx context.Context, cfg runConfig) (listOfQueries []st
 }
 
 // writeHeaderRow writes a header row to the supplied CSV file, which will be used for the output.
-func writeHeaderRow(csvFile *os.File, ctx context.Context) {
+func writeHeaderRow(csvFile *os.File, ctx context.Context) error {
 	headerRow := []string{
 		"NLP on or off?",
 		"Date and time of request",
@@ -212,11 +263,9 @@ func writeHeaderRow(csvFile *os.File, ctx context.Context) {
 	csvWriter := csv.NewWriter(csvFile)
 	defer csvWriter.Flush()
 	err := csvWriter.Write(headerRow)
-	check(ctx, "failed writing header row", err)
-}
-
-func check(ctx context.Context, msg string, err error) {
 	if err != nil {
-		log.Fatal(ctx, msg, err)
+		log.Error(ctx, "failed writing header row", err, log.Data{"headerRow": headerRow})
+		return err
 	}
+	return nil
 }
